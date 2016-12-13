865a13e2e4dab1a37c421fecdbf2ab8f7417e879
MarcoErice <marco.erice@gmail.com>
2016-12-13 18:40:57 +0100

    footer.js added

---
+++ scripts/footer.js
@@ -0,0 +1,7 @@
+﻿document.write("<footer class=\"container-fluid text-center\">");
+document.write("        <p>Online Store Copyright<\/p>");
+document.write("        <form class=\"form-inline\">Get deals:");
+document.write("            <input type=\"email\" class=\"form-control\" size=\"50\" placeholder=\"Email Address\">");
+document.write("            <button type=\"button\" class=\"btn btn-danger\">Sign Up<\/button>");
+document.write("        <\/form>");
+document.write("    <\/footer>");
