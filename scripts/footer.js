342faeb3dde1f8883212bd3ac2b7872bba0c7f48
MarcoErice <marco.erice@gmail.com>
2016-12-13 21:54:34 +0100

    footer script on index also

--- scripts/footer.js
+++ scripts/footer.js
@@ -1,5 +1,5 @@
 ﻿document.write("<footer class=\"container-fluid text-center\">");
-document.write("        <p>Online Store Copyright<\/p>");
+document.write("        <p>Marco Erice Copyright 2016<\/p>");
 document.write("        <form class=\"form-inline\">Get deals:");
 document.write("            <input type=\"email\" class=\"form-control\" size=\"50\" placeholder=\"Email Address\">");
 document.write("            <button type=\"button\" class=\"btn btn-danger\">Sign Up<\/button>");
